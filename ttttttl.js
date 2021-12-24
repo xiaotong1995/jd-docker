c73c2253a5cef796682f427f4aabe1fc627c7133
xiaotong1995 <61108909+xiaotong1995@users.noreply.github.com>
2021-12-24 12:25:19 +0800

    Update ttttttl.js

--- ttttttl.js
+++ ttttttl.js
@@ -1,5 +1,5 @@
 
-const $ = new Env('太太乐库存');
+const $ = new Env('太太乐');
 var request = require("request");
 let status;
 status = (status = ($.getval("ttlstatus") || "1") ) > 1 ? `${status}` : ""; // 账号扩展字符
